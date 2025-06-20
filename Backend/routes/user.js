d7f9436740d6b0eafb02e77727b90df09dc10098
SatyaKrishna12 <satyakrishna883@gmail.com>
2025-06-20 20:59:42 +0530

    Third commit

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -39,8 +39,8 @@ router.post("/login", async (req, res) => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    secure: false, 
-    sameSite: 'Lax',
+    secure: true, 
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000, 
   });
 
